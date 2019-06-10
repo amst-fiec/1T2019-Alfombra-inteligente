cf7ef56f6b13b9fb1d53b28c738578155b0cdd3e
Carlos Cedeño <caencede@espol.edu.ec>
2019-06-09 19:48:31 -0500

    Tarea autonoma 2

---
+++ app/src/main/java/com/example/carlo/amst5/menu.java
@@ -0,0 +1,26 @@
+package com.example.carlo.amst5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+public class menu extends AppCompatActivity {
+    String token = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Intent login = getIntent();
+        this.token = (String)login.getExtras().get("token");
+    }
+    public void Salir(View v){
+        this.finish();
+        System.exit(0);
+    }
+    public void revisarSensores(View v){
+        Intent red_sensores = new Intent(getBaseContext(),
+                redes_sensores.class);
+        red_sensores.putExtra("token", token);
+        startActivity(red_sensores);
+    }
+}
---
+++ app/src/main/java/com/example/carlo/amst5/redes_sensores.java
@@ -0,0 +1,182 @@
+package com.example.carlo.amst5;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+public class redes_sensores extends AppCompatActivity {
+    private RequestQueue mQueue;
+    private String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJ1c2VybmFtZSI6ImNhcmxvc2MiLCJleHAiOjE1NTk5MjgzOTksImVtYWlsIjoiIn0.su6cuz6OCBIsoJj0-xULTxLKytiCaTlWa6aWjzGNv5o";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redes_sensores);
+        mQueue = Volley.newRequestQueue(this);
+        Intent login = getIntent();
+        this.token = (String)login.getExtras().get("token");
+        revisarSensores();
+    }
+
+
+
+    public void HttpPost(View v) throws  JSONException {
+        String url_temp = "https://amstdb.herokuapp.com/db/logUno/1";
+        JSONObject jsonObject = new JSONObject();
+        final TextView nueva_temp = (TextView)findViewById(R.id.txt_nueva_temperatura);
+        try {
+            // user_id, comment_id,status
+            jsonObject.put("key", "temperatura");
+            jsonObject.put("value", nueva_temp.getText());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.PUT, url_temp, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        try {
+                            nueva_temp.setText(response.getString("value"));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "JWT " + token);
+                System.out.println(token);
+                return params;
+            }
+        };;
+        mQueue.add(request);
+
+    }
+
+
+
+    private void revisarSensores(){
+
+
+        final TextView tempValue = (TextView) findViewById(R.id.tempVal);
+        final TextView pesoValue = (TextView) findViewById(R.id.pesoVal);
+        final TextView humedadValue = (TextView)findViewById(R.id.humedadVal);
+
+        String url_temp = "https://amstdb.herokuapp.com/db/logUno/1";
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET, url_temp, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        try {
+                            tempValue.setText(response.getString("value"));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "JWT " + token);
+                System.out.println(token);
+                return params;
+            }
+        };;
+        mQueue.add(request);
+        String url_humedad = "https://amstdb.herokuapp.com/db/logUno/2";
+        JsonObjectRequest request_humedad = new JsonObjectRequest(
+                Request.Method.GET, url_humedad, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        try {
+                            humedadValue.setText(response.getString("value"));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "JWT " + token);
+                System.out.println(token);
+                return params;
+            }
+        };;
+        mQueue.add(request_humedad);
+        String url_peso = "https://amstdb.herokuapp.com/db/logUno/3";
+        JsonObjectRequest request_peso = new JsonObjectRequest(
+                Request.Method.GET, url_peso, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        try {
+                            pesoValue.setText(response.getString("value"));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "JWT " + token);
+                System.out.println(token);
+                return params;
+            }
+        };;
+        mQueue.add(request_peso);
+
+
+    }
+}
